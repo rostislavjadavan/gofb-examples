7dff97315c095da33520244c014b8433f9f00a64
Rostislav Jadavan <rostislav@jadavan.com>
2020-04-16 22:40:49 +0200

    simple example rotation

--- simple/simple.go
+++ simple/simple.go
@@ -7,10 +7,10 @@ import (
 
 func main() {
 	w := gofb.NewWindow("go-fb", 1200, 900, false)
-	bg := gofb.NewSurface(1200, 900)
+	bg := gofb.NewSurface(600, 600)
 
-	for y := 200; y < 700; y++ {
-		for x := 200; x < 1000; x++ {
+	for y := 0; y < bg.Height; y++ {
+		for x := 0; x < bg.Width; x++ {
 			gray := uint8(rand.Intn(255))
 			bg.SetPixel(x, y, gofb.NewColor(gray, gray, gray, 255))
 		}
@@ -20,7 +20,8 @@ func main() {
 		w.StartFrame()
 		w.Clear(gofb.NewColor(0, 0, 0, 255))
 
-		bg.Draw(0, 0)
+		bg.Draw(300, 150)
+		bg.Rotation += float32(w.GetDeltaTimeMs() / 10)
 
 		w.FinalizeFrame()
 	}
